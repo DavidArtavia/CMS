beb8320bbf555bd5b94b6fc9a3074076194f8fbe
david <darias911@gmail.com>
2021-11-26 18:33:36 -0600

    Cambios 26/11/20221

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,7 @@ Route::get('/', function () {
     return view('plantilla');
 });
 
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
-
-
-
