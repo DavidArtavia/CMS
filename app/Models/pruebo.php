1907a9340e3e0235303673d46b31db9bbb24444b
david <darias911@gmail.com>
2021-11-26 18:37:03 -0600

    Se cambio plantilla

---
+++ app/Models/pruebo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pruebo extends Model
+{
+    use HasFactory;
+}
---
+++ resources/views/modules/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>We are the champions</b> 
+    </div>
+    <strong>Copyright  © 
+        {{-- Sintaxis PHP para recuperar año --}}
+        @php
+            echo date('Y');
+        @endphp
+        <a href="https://google.com">Artavia</a>.</strong> All rights reserved.
+</footer>
